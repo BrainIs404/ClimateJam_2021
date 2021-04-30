01388d9921c64cced5d70ce90de88db0d07f77ed
VictorNgo <tminhngvt1998@gmail.com>
2021-04-30 23:11:51 +1000

    make player move along set path

---
+++ Assets/Script/Node.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+    public string GetName()
+    {
+        return this.name;
+    }
+}
---
+++ Assets/Script/PathFollower.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class PathFollower : MonoBehaviour {
+    Node[] PathNode;
+    public GameObject Player;
+    public float MoveSpeed;
+    float Timer;
+
+    int CurrentNode;
+    Vector2 startPosition;
+
+    bool isMove = false;
+    static Vector3 CurrentPositionHolder;
+    void Start() {
+        PathNode = GetComponentsInChildren<Node>();
+        Debug.Log(PathNode);
+        CheckNode();
+        
+    }
+
+    void CheckNode() {
+        Timer = 0;
+        startPosition = Player.transform.position;
+        CurrentPositionHolder = PathNode[CurrentNode].transform.position;
+    }
+
+
+    void DrawLine() {
+        for (int i = 0; i < PathNode.Length - 1; i++) {
+            Vector2 currentPos = PathNode[i].transform.position;
+            Vector2 nextPos = PathNode[i+1].transform.position;
+            Debug.DrawLine(currentPos, nextPos, Color.blue);
+        }
+    }
+
+    void Update() {
+        DrawLine();
+        if (Input.GetMouseButtonDown(0)) {
+            isMove = !isMove;
+        }
+
+        if (isMove) {
+            
+            Debug.Log(CurrentNode);
+            Timer += Time.deltaTime * MoveSpeed;
+
+            if (Player.transform.position != CurrentPositionHolder) {
+                Player.transform.position = Vector3.Lerp(startPosition, CurrentPositionHolder, Timer);
+            } else {
+                if (CurrentNode < PathNode.Length - 1) {
+                    CurrentNode++;
+                    CheckNode();
+                } else {
+                    isMove = !isMove;
+                }
+            }
+        }
+    }
+}
